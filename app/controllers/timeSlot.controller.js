6e8fb94e4c8118b7556a84daf0a2ac58be06fb05
JIshanRandika <jishanrandika@gmail.com>
2023-02-23 15:50:28 +0530

    update time slot and get

--- app/controllers/timeSlot.controller.js
+++ app/controllers/timeSlot.controller.js
@@ -39,3 +39,90 @@ exports.timeSlots = (req, res) => {
         });
     });
 };
+
+// get timeSlots1InLecHallInDay
+exports.timeSlots1InLecHallInDay =  (req, res) => {
+    TimeSlot.find({'hallName': req.body.hallName,'lecDate': req.body.lecDate,'timeSlotID':"1"}).select('-__v').then(itemInfos => {
+        res.status(200).json(itemInfos);
+    }).catch(error => {
+        // log on console
+        console.log(error);
+
+        res.status(500).json({
+            message: "Error!",
+            error: error
+        });
+    });
+}
+
+// get timeSlots2InLecHallInDay
+exports.timeSlots2InLecHallInDay =  (req, res) => {
+    TimeSlot.find({'hallName': req.body.hallName,'lecDate': req.body.lecDate,'timeSlotID':"2"}).select('-__v').then(itemInfos => {
+        res.status(200).json(itemInfos);
+    }).catch(error => {
+        // log on console
+        console.log(error);
+
+        res.status(500).json({
+            message: "Error!",
+            error: error
+        });
+    });
+}
+
+// get timeSlots3InLecHallInDay
+exports.timeSlots3InLecHallInDay =  (req, res) => {
+    TimeSlot.find({'hallName': req.body.hallName,'lecDate': req.body.lecDate,'timeSlotID':"3"}).select('-__v').then(itemInfos => {
+        res.status(200).json(itemInfos);
+    }).catch(error => {
+        // log on console
+        console.log(error);
+
+        res.status(500).json({
+            message: "Error!",
+            error: error
+        });
+    });
+}
+
+// get timeSlots4InLecHallInDay
+exports.timeSlots4InLecHallInDay =  (req, res) => {
+    TimeSlot.find({'hallName': req.body.hallName,'lecDate': req.body.lecDate,'timeSlotID':"4"}).select('-__v').then(itemInfos => {
+        res.status(200).json(itemInfos);
+    }).catch(error => {
+        // log on console
+        console.log(error);
+
+        res.status(500).json({
+            message: "Error!",
+            error: error
+        });
+    });
+}
+
+
+exports.updateTimeSlotsInLecHallInDay = (req, res) => {
+
+    TimeSlot.findOneAndUpdate(
+        { hallName: req.body.hallName, lecDate: req.body.lecDate, timeSlotID: req.body.timeSlotID },
+        {
+            status: req.body.status,
+        },
+        {new: true}
+    ).select('-__v')
+        .then(item => {
+            if(!item) {
+                return res.status(404).send({
+                    message: "Error -> Can NOT update a order with id = " + req.params.id,
+                    error: "Not Found!"
+                });
+            }
+
+            res.status(200).json(item);
+        }).catch(err => {
+        return res.status(500).send({
+            message: "Error -> Can not update a order with id = " + req.params.id,
+            error: err.message
+        });
+    });
+}
--- app/routes/timeSlot.router.js
+++ app/routes/timeSlot.router.js
@@ -4,6 +4,11 @@ module.exports = function(app) {
 
     app.post('/api/addATimeSlot', timeSlots.addATimeSlot);
     app.get('/api/getTimeSlots', timeSlots.timeSlots);
+    app.post('/api/timeSlots1InLecHallInDay', timeSlots.timeSlots1InLecHallInDay);
+    app.post('/api/timeSlots2InLecHallInDay', timeSlots.timeSlots2InLecHallInDay);
+    app.post('/api/timeSlots3InLecHallInDay', timeSlots.timeSlots3InLecHallInDay);
+    app.post('/api/timeSlots4InLecHallInDay', timeSlots.timeSlots4InLecHallInDay);
+    app.put('/api/updateTimeSlotsInLecHallInDay', timeSlots.updateTimeSlotsInLecHallInDay);
 
 
 }
