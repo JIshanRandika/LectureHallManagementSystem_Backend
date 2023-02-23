5899e52d2c29d285deeeab38857465fecd1cdb52
JIshanRandika <jishanrandika@gmail.com>
2023-02-23 08:49:01 +0530

    update

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/logout", logoutRoutes);
 
 require('./app/routes/lecHall.router')(app)
+require('./app/routes/timeSlot.router')(app)
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
